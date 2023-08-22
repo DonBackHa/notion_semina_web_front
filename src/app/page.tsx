b47a38c995120bb4c14e1239dcf4fb317344835e
lifeoasis-sera <sera.2021@lifeoasis.com>
2023-08-22 20:46:46 +0900

    feat : get DB rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+
+type DBRowType = {
+    id: string,
+    title: string,
+    description: string,
+    type: string,
+    complete: boolean,
+    date: string,
+}
+
 export default function Home() {
+    async function getDB() {
+        const {data} = await axios.get<DBRowType[]>('http://localhost:4000/rows')
+        return data
+    }
+
     return <main>
         <div>
             <text>{"notion DB ABC"}</text>
+            {getDB().then(rows =>{
+                return rows.map(row =>{
+                    return <div>
+                        <text>{row.title}</text>
+                    </div>
+                })
+            })}
         </div>
     </main>
 }
